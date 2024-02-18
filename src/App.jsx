aa8e66464cc7be65a12312838c815e066a8ad020
togay-aytemiz <togayaytemiz@gmail.com>
2024-02-18 14:19:39 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,10 @@ export default function App() {
   const [wordsDataArray, setWordsDataArray] = useState(wordsDataOne)
   const [currentWord, setCurrentWord] = useState(wordsDataArray[0])
 
-  /* Challenge 
 
-    Aşağıdaki iki <NavButtons /> ve bir <Word /> elementinde çok fazla prop var. Sizin göreviniz kodun bu kısmını aşağıdaki gibi daha kısa ve etkili hale getirmektir: 
-        
-        1. Üç öğenin (iki <NavButtons /> ve bir <Word /> öğesi) her birinde yalnızca bir prop olmalıdır. Her prop, o anda elemana aktarılmakta olan tüm verileri korumalıdır. 
-                        
-        2. Components klasöründeki NavButtons ve Word bileşenlerinin kodu, propların öğelere nasıl aktarıldığındaki değişikliklere uyum sağlayacak şekilde değiştirilmelidir. Yapmanız gereken değişiklikler *çok* küçük değişiklikler.   
-	
-        3. Bu görevleri minimum miktarda çalışmayla gerçekleştirmelisiniz. Bu görevi tamamlamak için yapmanız gereken fazla bir şey yok. Kendinizi çok sıkıcı, tekrarlayan işler yaparken bulursanız, bu görevlerden birini verimsiz bir şekilde yaptığınızın işaretidir  
-		   
-	Not: Sonuçların, prop'ları geçmenin "en iyi" yolu olması gerekmez. Bu sadece geçirdiğiniz prop sayısını azaltmaya yönelik bir alıştırma olarak düşünülmüştür
-*/
 
   const data = {wordsDataArray, currentWord, setCurrentWord }
-  
+
   return (
     <div className='wrapper'>
       <Header />
@@ -46,3 +35,18 @@ export default function App() {
     </div>
   )
 }
+
+
+
+  /* Challenge 
+
+    Aşağıdaki iki <NavButtons /> ve bir <Word /> elementinde çok fazla prop var. Sizin göreviniz kodun bu kısmını aşağıdaki gibi daha kısa ve etkili hale getirmektir: 
+        
+        1. Üç öğenin (iki <NavButtons /> ve bir <Word /> öğesi) her birinde yalnızca bir prop olmalıdır. Her prop, o anda elemana aktarılmakta olan tüm verileri korumalıdır. 
+                        
+        2. Components klasöründeki NavButtons ve Word bileşenlerinin kodu, propların öğelere nasıl aktarıldığındaki değişikliklere uyum sağlayacak şekilde değiştirilmelidir. Yapmanız gereken değişiklikler *çok* küçük değişiklikler.   
+	
+        3. Bu görevleri minimum miktarda çalışmayla gerçekleştirmelisiniz. Bu görevi tamamlamak için yapmanız gereken fazla bir şey yok. Kendinizi çok sıkıcı, tekrarlayan işler yaparken bulursanız, bu görevlerden birini verimsiz bir şekilde yaptığınızın işaretidir  
+		   
+	Not: Sonuçların, prop'ları geçmenin "en iyi" yolu olması gerekmez. Bu sadece geçirdiğiniz prop sayısını azaltmaya yönelik bir alıştırma olarak düşünülmüştür
+*/
